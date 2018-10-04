c7c20382a3c18397df0f4288748a23b4300f92bb
Maykonn <maykonn@outlook.com>
2018-10-04 12:11:00 -0300

    Helper to work with string, for example, a implementation of Java hashCode algorithm

---
+++ src/helper/StringHelper.js
@@ -0,0 +1,20 @@
+class StringHelper {
+
+  /**
+   * This matches Java's implementation of the standard object.hashCode()
+   *
+   * @param str
+   * @return {number}
+   * @see https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript/33647870#33647870
+   */
+  static hashCode(str) {
+    let hash = 0, i = 0, len = str.length;
+    while (i < len) {
+      hash = ((hash << 5) - hash + str.charCodeAt(i++)) << 0;
+    }
+    return (hash + 2147483647) + 1;
+  }
+
+}
+
+module.exports = StringHelper;
